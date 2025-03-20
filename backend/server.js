df7a082a0ce79b30c7cb0b137bd052d6cb73554c
praisedp <pasandp2k3@gmail.com>
2025-03-20 11:53:54 +0530

    fixed server.js error

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,20 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/mongodb.js';
+import authRoutes from "./routes/authRoutes.js";
+import incomeRoutes from "./routes/incomeRoutes.js";
+import expenseRoutes from "./routes/expenseRoutes.js";
+import dashboardRoutes from "./routes/dashboardRoutes.js";
+import financialRoutes from './routes/financialRoutes.js';
+import transactionRoutes from "./routes/transactionRoutes.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // Load environment variables
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // App Config
 const app = express();
@@ -21,7 +32,7 @@ import memberApplicationRoutes from '../backend/routes/memberApplicationRoutes.j
 app.use('/api/member-applications', memberApplicationRoutes);
 import adminApplicationRoutes from './routes/adminApplicationRoutes.js';
 app.use('/api/admin/applications', adminApplicationRoutes);
-// Login Routes
+// User Routes
 import userRoutes from './routes/userRoutes.js';
 app.use('/api/users', userRoutes);
 
@@ -31,6 +42,13 @@ import organizerRoutes from './routes/organizerRoutes.js';
 // Mount the organizer routes
 app.use('/api/organizers', organizerRoutes);
 
+
+app.use('/api/finance', financialRoutes);
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/income", incomeRoutes);
+app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+app.use("/api/v1/transaction", transactionRoutes);
 
 // API Endpoints
 app.get('/register/member/application', (req, res) => {
