c459d45c7a1ab852ff3e04b916db9d23d9416fdc
Vishwa Perera <vishwaperera@Vishwas-MacBook-Pro.local>
2025-03-22 12:12:54 +0530

    Updated navbar and home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,17 @@ import Homepage from './pages/Home.jsx';
 import AboutUs from './pages/AboutUs';
 import Contactus from './pages/Contactus';
 import Profile from './pages/Profile';
+import PackageManager from './pages/events/PackageManager';
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-0 md:px-0 lg:px-0 mx-auto">
-      <Navbar />
+    <div className="px-4 sm:px-0 md:px-0 lg:px-0 mx-auto -z-10">
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/aboutUs" element={<AboutUs />} />
         <Route path="/contactUs" element={<Contactus />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/packages" element={<PackageManager />} />
       </Routes>
     </div>
   );
