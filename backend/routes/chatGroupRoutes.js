88bf77d582725a47055eb1920bec6445259d9d27
praisedp <pasandp2k3@gmail.com>
2025-03-26 21:25:27 +0530

    usermanagement fine tuning

--- backend/routes/chatGroupRoutes.js
+++ backend/routes/chatGroupRoutes.js
@@ -1,7 +1,7 @@
 // File: /backend/routes/chatGroupRoutes.js
 import express from 'express';
 const router = express.Router();
-import { createChatGroup, updateChatGroupMembers, deleteChatGroup, getChatGroupsForUser, getAllChatGroups } from '../controllers/chatGroupController.js';
+import { createChatGroup, updateChatGroupMembers, deleteChatGroup, getChatGroupsForUser, getAllChatGroups, getGroupMembers, addMembersToGroup, removeMemberFromGroup } from '../controllers/chatGroupController.js';
 
 // Create a new chat group (manager only)
 router.post('/', createChatGroup);
@@ -17,4 +17,9 @@ router.get('/user/:userId', getChatGroupsForUser);
 
 
 router.get('/all', getAllChatGroups);
+
+router.get('/:groupId/members', getGroupMembers);
+
+router.put('/:groupId/members/remove/:memberId', removeMemberFromGroup);
+router.put('/:groupId/members/add', addMembersToGroup);
 export default router;
