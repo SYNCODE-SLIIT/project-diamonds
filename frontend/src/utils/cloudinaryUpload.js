8c1184b02e339d296781c6a68be6053296ac80ed
NPraveenLiyanage <nipunaliyange44@gmail.com>
2025-03-27 01:53:37 +0530

    updated financial management

---
+++ frontend/src/utils/cloudinaryUpload.js
@@ -0,0 +1,17 @@
+import streamifier from "streamifier";
+
+export const uploadToCloudinary = (buffer) => {
+  return new Promise((resolve, reject) => {
+    const stream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto" },
+      (error, result) => {
+        if (result) {
+          resolve(result);
+        } else {
+          reject(error);
+        }
+      }
+    );
+    streamifier.createReadStream(buffer).pipe(stream);
+  });
+};
