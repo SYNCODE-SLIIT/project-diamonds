ec3b28a9d7256767bb9da8bf7dacbc13880e6a6c
ruvinducode <ruvindurock1@gmail.com>
2025-05-08 12:16:56 +0530

    side bar edit for team management

--- frontend/src/components/layout/AdminSidebar.jsx
+++ frontend/src/components/layout/AdminSidebar.jsx
@@ -646,14 +646,14 @@ const AdminSidebar = () => {
                   </li>
                         <li className="mb-[15px]">
                 <NavLink
-                  to="/admin/event-calendar"
+                  to="/admin/practice-assign"
                   className={({ isActive }) =>
                     `${isActive ? 'bg-[rgba(79,70,229,0.25)] font-bold' : ''} 
                     flex items-center gap-[10px] text-white no-underline text-[16px] p-[10px] rounded-[8px]
                     transition-colors duration-300 ease hover:bg-[rgba(79,70,229,0.15)]`
                   }
                 >
-                  Calendar
+                 Practices
                 </NavLink>
               </li>
                   <li>
